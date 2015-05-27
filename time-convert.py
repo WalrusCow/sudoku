3cb940077c3a48bcab16d99aee86bf7064b5be52
WalrusCow <willgmcd@gmail.com>
2015-05-27 15:30:35 -0400

    time convert script lol

---
+++ time-convert.py
@@ -0,0 +1,10 @@
+import sys
+import re
+for l in sys.stdin:
+    start = re.search('\d', l).start()
+    l = l[start:]
+    m = l.index('m')
+    mins = int(l[:m])
+    dot = l.index('.')
+    secs = int(l[m+1:dot])
+    print(str(mins*60 + secs) + l[dot:-1])
